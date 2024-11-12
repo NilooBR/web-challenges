3a7473a3cd4c02fe833d61287d719f154eb55908
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-12 14:51:25 +0100

    Input to Uppercase and Lowercase

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,13 @@
 console.clear();
+
+const inputField = document.querySelector('[data-js="first-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+
+uppercaseButton.addEventListener("click", () => {
+  inputField.value = inputField.value.toUpperCase();
+});
+
+lowercaseButton.addEventListener("click", () => {
+  inputField.value = inputField.value.toLowerCase();
+});

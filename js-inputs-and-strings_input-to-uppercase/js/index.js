4ab80cbe7019aa10889b561f1fd37e6dd91abf06
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-12 14:36:11 +0100

    Input to Uppercase

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+
+const inputField = document.querySelector('[data-js="first-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+
+
+uppercaseButton.addEventListener("click", () => {
+  inputField.value = inputField.value.toUpperCase();
+});

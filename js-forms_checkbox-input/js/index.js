a1f5464f3aad497f382f93ef7c8bfffd51d40e2d
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-13 11:57:01 +0100

    check box challenge

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,6 +3,8 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
+
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +14,36 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccessMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
+
+hideTosError();
+hideSuccessMessage();
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
+  if (!tosCheckbox.checked) {
+    showTosError();
+    hideSuccessMessage(); 
+    return;
+  }
 
-  // --^-- write your code here --^--
+  showSuccessMessage();
+  hideTosError(); 
 
-  // eslint-disable-next-line no-alert
   alert("Form submitted");
 });

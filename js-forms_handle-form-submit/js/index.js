af41179ce97f60bb73d5e164ff24d0b97da41ebb
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-13 11:37:36 +0100

    handle form submit

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,42 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit" , (evt) => {
+    evt.preventDefault();
+    const forElements = evt.target.elements;
+    const data = {
+        firstName : forElements.firstName.value,
+        lastName : forElements.lastName.value,
+        age : forElements.age.value,
+        email : forElements.email.value,
+        complaint : forElements.complaint.value,
+        details : forElements.details.value,
+        badness : forElements.details.value,
+        orderDate : forElements.orderDate.value,
+        datPrivacyCheckBox : forElements.tos.value,
+    };
+
+    console.log("data: ", data);
+});
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     const data = Object.fromEntries(formData);
+//     console.log("data", data);
+// });
+
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const formElements = evt.target.elements;
+    const data = {
+        firstName: formElements.firstName.value,
+        age: parseFloat(formElements.age.value),
+        badness: parseFloat(formElements.badness.value)
+    };
+    const ageBadnessSum = data.age + data.badness;
+
+    console.log(`The agebadnesssum of "${data.firstName}" is "${ageBadnessSum}"`);
+});

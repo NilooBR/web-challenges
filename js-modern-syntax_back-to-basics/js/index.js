82b036af606f63b6db80039a6353ac8c4e0f6ec1
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-20 16:09:34 +0100

    Back to Basics

--- js-modern-syntax_back-to-basics/js/index.js
+++ js-modern-syntax_back-to-basics/js/index.js
@@ -3,15 +3,33 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
-export const getNameAndCountry = ({ name, country }) => [name, country];
+// export const getNameAndCountry = ({ name, country }) => [name, country];
 
-export const getRelocatedCity = (
-  city1,
-  city2 = { name: "Berlin", country: "Germany" }
-) => {
-  const [, country] = getNameAndCountry(city2);
-  return {
-    ...city1,
-    country,
-  };
+// export const getRelocatedCity = (
+//   city1,
+//   city2 = { name: "Berlin", country: "Germany" }
+// ) => {
+//   const [, country] = getNameAndCountry(city2);
+//   return {
+//     ...city1,
+//     country,
+//   };
+// };
+
+
+export const getNameAndCountry = function(city) {
+  var name = city.name;
+  var country = city.country;
+  return [name, country];
+};
+
+export const getRelocatedCity = function(city1, city2) {
+  if (city2 === undefined) {
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+  var country = getNameAndCountry(city2)[1];
+  var updatedCity = {};
+  updatedCity.name = city1.name;
+  updatedCity.country = country;
+  return updatedCity;
 };

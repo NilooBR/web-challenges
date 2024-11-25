dca984920e92c1c1628905fa438544db62da11b6
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-25 15:14:44 +0100

    Calculating the Sum

--- react-props_sum/src/App.jsx
+++ react-props_sum/src/App.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Sum valueA={2} valueB={3} />
+      <Sum valueA={100} valueB={234} />
+      <Sum valueA={24} valueB={17} />
+      <Sum valueA={12} valueB={28} />
+    </div>
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <div>
+      <span>
+        {valueA} + {valueB} = {valueA + valueB}
+      </span>
+    </div>
+  );
 }

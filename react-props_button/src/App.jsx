ba7e495bcee50b57d76e222d2f2eb86cb5c2e7dc
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-25 15:40:55 +0100

    Buttons challenge

--- react-props_button/src/App.jsx
+++ react-props_button/src/App.jsx
@@ -2,5 +2,37 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div className="App">
+      <Button color="blue" disabled={false} text="Click Me" />
+      <Button color="red" disabled={true} text="Disabled" />
+      <Button color="green" disabled={false} text="Click Me" />
+      <Button color="purple" disabled={false} text="Click Me" />
+    </div>
+  );
+}
+
+function Button({ color, disabled, text }) {
+  const handleClick = () => {
+    if (!disabled) {
+      console.log(`Button clicked: ${text}`);
+    }
+  };
+  return (
+    <button
+      style={{
+        color: "black",
+        backgroundColor: disabled ? "#d3d3d3" : color,
+        height: "50px",
+        borderRadius: "5px",
+        padding: "20px",
+        margin:"5px",
+        cursor: disabled ? "not-allowed" : "pointer",
+      }}
+      disabled={disabled}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
 }

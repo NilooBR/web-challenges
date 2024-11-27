2406bd5b7495d53ef9c7f524990e9aabe8eb16fa
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-27 10:16:39 +0100

    React Arrays: Fruits

--- react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,13 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ id, name, color, image }) {
+  const colorClass = `card--${color.toLowerCase()}`;
+
+  return (
+    <div className={`card ${colorClass}`}>
+      <h2>{name}</h2>
+      <p>Color: {color}</p>
+      <img src={image} alt={name} />
+    </div>
+  );
 }

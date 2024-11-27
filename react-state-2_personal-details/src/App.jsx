1aedf671fc052dba66444a18f9c74595598d314c
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-27 16:32:00 +0100

    React State 2: Personal Details

--- react-state-2_personal-details/src/App.jsx
+++ react-state-2_personal-details/src/App.jsx
@@ -1,17 +1,24 @@
-import "./styles.css";
+import { useState } from "react";
 import Form from "./components/Form";
+import "./styles.css";
 
 export default function App() {
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  function handleCreateUser(name, email) {
+    setUser({ name, email });
+  }
+
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form onCreateUser={handleCreateUser} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John Doe</span>
+        Name: <span className="output">{user.name || ""}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{user.email || ""}</span>
       </p>
     </div>
   );

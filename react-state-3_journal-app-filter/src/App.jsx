e436c20317e58ac7ce72d6e4559a3d47b99c4b62
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-11-28 22:22:12 +0100

    React State: Journal App with Entries Filter

--- react-state-3_journal-app-filter/src/App.jsx
+++ react-state-3_journal-app-filter/src/App.jsx
@@ -13,6 +13,7 @@ const initialEntries = [
     motto: "We are in a state of chaos",
     notes:
       "Today I learned about React State. It was fun! I can't wait to learn more.",
+    isFavorite: false,
   },
   {
     id: 999,
@@ -20,6 +21,7 @@ const initialEntries = [
     motto: "Props, Props, Props",
     notes:
       "Today I learned about React Props. Mad props to everyone who understands this!",
+    isFavorite: true,
   },
   {
     id: 998,
@@ -27,17 +29,22 @@ const initialEntries = [
     motto: "How to nest components online fast",
     notes:
       "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    isFavorite: false,
   },
   {
     id: 997,
     date: "Feb 2, 2025",
     motto: "I'm a React Developer",
     notes: "My React-ion when I learned about React: Yay!",
+    isFavorite: true,
   },
 ];
 
 function App() {
   const [entries, setEntries] = useState(initialEntries);
+  const [filter, setFilter] = useState("all");
+
+  const favoriteEntries = entries.filter((entry) => entry.isFavorite);
 
   function handleAddEntry(newEntry) {
     const date = new Date().toLocaleDateString("en-us", {
@@ -54,14 +61,27 @@ function App() {
     );
   }
 
+  function handleShowAllEntries() {
+    setFilter("all");
+  }
+
+  function handleShowFavoriteEntries() {
+    setFilter("favorites");
+  }
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
         <EntryForm onAddEntry={handleAddEntry} />
         <EntriesSection
-          entries={entries}
+          entries={filter === "all" ? entries : favoriteEntries}
           onToggleFavorite={handleToggleFavorite}
+          onShowAllEntries={handleShowAllEntries}
+          onShowFavoriteEntries={handleShowFavoriteEntries}
+          filter={filter}
+          allEntriesCount={entries.length}
+          favoriteEntriesCount={favoriteEntries.length}
         />
       </main>
       <Footer />

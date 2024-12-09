4eaa5e2579347e4392bd4d4d2e20a851b6da51c4
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-09 15:57:14 +0100

    Card Conversion

--- react-styled-components_card-conversion/components/Card/index.js
+++ react-styled-components_card-conversion/components/Card/index.js
@@ -1,13 +1,18 @@
+import Container from "../Container";
+import Description from "../Description";
+import Title from "../Title";
+
+
 export default function Card() {
   return (
-    <div className="card">
-      <h3 className="card__title">Homer Simpson</h3>
-      <p className="card__description">
+    <Container>
+      <Title>Homer Simpson</Title>
+      <Description>
         Homer Jay Simpson is a fictional character and the main protagonist of
         the American animated sitcom The Simpsons. He is voiced by Dan
         Castellaneta and first appeared, along with the rest of his family, in
         The Tracey Ullman Show short Good Night on April 19, 1987.
-      </p>
-    </div>
+      </Description>
+    </Container>
   );
 }

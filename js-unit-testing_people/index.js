2928a22d77db32e2fe064dd66bd8372ace31224c
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-10 11:59:15 +0100

    JS Unit Testing with People

--- js-unit-testing_people/index.js
+++ js-unit-testing_people/index.js
@@ -1,19 +1,59 @@
+import { people } from "./data.js";
 export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+console.log(getFirstNames(people));
 
-export function getNameAndAge(people) {}
+export function getFullNames(people) {
+  return people.map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getPeopleByAge(people, age) {}
+console.log(getFullNames(people));
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
-export function getPeopleByLastName(people, lastName) {}
+console.log(getNameAndAge(people));
 
-export function findPersonById(people, id) {}
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => person.age === age);
+}
 
-export function isAnyoneOlderThan(people, age) {}
+console.log(getPeopleByAge(people, 38));
 
-export function getFullNamesSortedByAge(people) {}
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => person.age > age)
+    .map((person) => `${person.firstName} ${person.lastName}`);
+}
+
+console.log(getPeopleNamesOlderThan(people, 23));
+
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person) => person.lastName === lastName);
+}
+
+console.log(getPeopleByLastName(people, "Musterfrau"));
+
+export function findPersonById(people, id) {
+  return people.find((person) => person.id === id);
+}
+
+console.log(findPersonById(people, 3));
+
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > age);
+}
+
+console.log(isAnyoneOlderThan(people, 55));
+console.log(isAnyoneOlderThan(people, 23));
+
+export function getFullNamesSortedByAge(people) {
+  return people
+    .toSorted((a, b) => a.age - b.age)
+    .map((person) => person.lastName);
+}
+
+console.log(getFullNamesSortedByAge(people));

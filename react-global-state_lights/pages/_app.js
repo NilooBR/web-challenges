78118f0f388dc3a7332c7acee37ff1170f6c48af
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-11 17:11:50 +0100

    work on toggle

--- react-global-state_lights/pages/_app.js
+++ react-global-state_lights/pages/_app.js
@@ -1,11 +1,31 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
+const initialLights = [
+  { id: 0, name: "Wohnzimmer", isOn: false },
+  { id: 1, name: "Küche", isOn: false },
+  { id: 2, name: "Schlaffzimmer", isOn: true },
+  { id: 3, name: "Badezimmer", isOn: false },
+];
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initialLights);
+
+  function HandleToggle(id) {
+    setLights((lights) =>
+      lights.map((light) => {
+        if (light.id === id) {
+          return { ...light, isOn: !light.isOn };
+        }
+        return light;
+      })
+    );
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} lights={lights} onToggle={HandleToggle} />
     </Layout>
   );
 }

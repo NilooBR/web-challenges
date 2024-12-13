aa027a9bd659183ca2fcc560db8bd1e3d5032731
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-13 11:36:29 +0100

    use SWR for ISSTracker

--- react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,36 +1,30 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+  const {
+    data: coords,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR(URL, fetcher, { refreshInterval: 5000 });
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+
+  if (isLoading || !coords) {
+    return <p>Loading...</p>;
   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  if (error) {
+    return <p>Something went wrong...</p>;
+  }
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  const getISSCoords = () => {
+    mutate();
+  };
 
   return (
     <main>

35db2b8affa85073f05e100761aebe6b148ea3cb
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-17 11:29:50 +0100

    Backend Basics: Greeting

--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,3 +1,13 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request,response)=>{
+
+    console.log("request.url: ", request.url);
+    console.log("request.method: ", request.method);
+  
+    if (request.url === "/Niloofar") {
+      response.statusCode = 200;
+      response.end("Hello Niloofar!!");
+      return;
+    } 
+})

52beb23029d2329575787a93104654b9dd9ed74f
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-19 12:28:08 +0100

    Backend Create: Products

--- backend-create_products/pages/api/products/index.js
+++ backend-create_products/pages/api/products/index.js
@@ -11,5 +11,23 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      const newProduct = await Product.create(productData);
+
+      console.log("Product created: ", newProduct);
+      response
+        .status(201)
+        .json({ status: "Product created", product: newProduct });
+    } catch (error) {
+      console.error("Error creating product:", error);
+      response
+        .status(500)
+        .json({ status: "Error creating product", error: error.message });
+    }
+    return;
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
